154ace192eceb01186eea65e0f7a2b30b4824187
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-05-26 09:53:43 +0530

    first

---
+++ src/main/java/com/learn/short_url_builder/ShortUrlBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.learn.short_url_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortUrlBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortUrlBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/short_url_builder/ShortUrlBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.short_url_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortUrlBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
