b1553543ba157370910d0b8537f30e795788f396
dhanushkasampath <dhanushka.2018410@iit.ac.lk>
2024-06-14 11:15:38 +0530

    SUB-002: successfully saved in mongodb

--- src/main/java/com/learn/short_url_builder/service/impl/ShortUrlServiceImpl.java
+++ src/main/java/com/learn/short_url_builder/service/impl/ShortUrlServiceImpl.java
@@ -3,9 +3,12 @@ package com.learn.short_url_builder.service.impl;
 import com.learn.short_url_builder.dto.GenerateUrlRequestDto;
 import com.learn.short_url_builder.dto.RemoveUrlRequestDto;
 import com.learn.short_url_builder.dto.ResponseUrlDto;
+import com.learn.short_url_builder.model.ShortUrlDocument;
 import com.learn.short_url_builder.repository.ShortUrlRepository;
 import com.learn.short_url_builder.service.ShortUrlService;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class ShortUrlServiceImpl implements ShortUrlService {
@@ -18,8 +21,19 @@ public class ShortUrlServiceImpl implements ShortUrlService {
 
     @Override
     public ResponseUrlDto generateShortUrl(GenerateUrlRequestDto generateUrlRequestDto) {
+
+        ShortUrlDocument shortUrlDocument = new ShortUrlDocument();
+        shortUrlDocument.setId(1L);
+        shortUrlDocument.setLongUrl(generateUrlRequestDto.getLongUrl());
+        shortUrlDocument.setShortUrl("https://rb.gy/090byc");
+        shortUrlDocument.setCreatedDate(LocalDateTime.now());
+        shortUrlDocument.setLastModifiedDate(LocalDateTime.now());
+        shortUrlDocument.setExpiryDate(LocalDateTime.now());
+        shortUrlRepository.save(shortUrlDocument);
+
         ResponseUrlDto responseUrlDto = new ResponseUrlDto();
         responseUrlDto.setShortUrl("https://rb.gy/090byc");
+
         return responseUrlDto;
     }
 
